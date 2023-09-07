fe7c49d8dab0931fb0caa3868d5814b4c7a750cf
vincent-panabieres <115145513+vincent-panabieres@users.noreply.github.com>
2023-09-07 14:34:12 +0200

    backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -1,13 +1,24 @@
-def paradox(tree):
-    rock = 0
-    step = tree / 2
-    tolerance = 1e-9
-
-    while abs(rock - tree) >= tolerance:
-        rock += step
-        step /= 2
-        print(f"Position of the rock: {rock}\nDistance for the step: {step}")
-
-    print("The rock reaches the tree!")
-
-paradox(8)
+create_backup() {
+    local source_dir="$1"
+    local backup_prefix="$2"
+    local timestamp=$(date +%d-%b-%Y)
+    local user="$3"
+    tar czf "$backup_dir/$user-$backup_prefix-$timestamp.tar.gz" -C "$source_dir" .
+}
+if [ ! -d "$backup_dir" ]; then
+    mkdir -p "$backup_dir"
+fi
+for user_home in /home/*; do
+    username=$(basename "$user_home")
+    user_backup_dir="$backup_dir/$username"
+    if [ ! -d "$user_backup_dir" ]; then
+        mkdir -p "$user_backup_dir"
+    fi
+    create_backup "$user_home" "fichiers-moins-7-jours" "$username"
+    create_backup "$user_home" "fichiers-plus-7-jours" "$username"
+    find "$user_home" -type d -exec du -sh {} + | awk -F $'\t' '$1 > 10M {print $2}' | while read -r large_dir; do
+        create_backup "$large_dir" "repertoires-plus-10Mo" "$username"
+    done
+    create_backup "$user_home" "fichiers-repertoires-caches" "$username"
+    chown -R "$username:$username" "$user_backup_dir"
+done
